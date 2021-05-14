b1d94b1edf1b53c0a0bc428f624c2de7b261f171
Karan <Kumarkay.ca@gmail.com>
2021-05-14 11:16:01 -0400

    Initial Commit

---
+++ Record/RecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordApp.swift
+//  Record
+//
+//  Created by Sandeep Jangra on 2021-05-14.
+//
+
+import SwiftUI
+
+@main
+struct RecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
